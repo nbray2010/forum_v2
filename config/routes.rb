7dfe23f6bdeb8e7ed6e080c6b0c27b9ace6ef247
Nick Bray <nbray2010@gmail.com>
2014-10-13 16:04:17 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ForumV2::Application.routes.draw do
+  resources :users
   root "static_pages#home"
   match '/signup',	to: 'users#new', 			via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
