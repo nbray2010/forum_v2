7fda3b99570f1c27a2262dfb39174ce017cd9b16
Nick Bray <nbray2010@gmail.com>
2014-11-05 16:53:45 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
+	attr_accessible :name, :email, :password, :password_confirmation
+
 	before_save { self.email = email.downcase }
+	before_save :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL }, uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	
+	private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
